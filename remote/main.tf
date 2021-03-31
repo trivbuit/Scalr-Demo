8081a677d77b61fc98ccf3e5c9ed6e8b48184011
Tri Van BUI <trivbuit@gmail.com>
2021-03-31 15:27:38 +0700

    Test'

--- remote/main.tf
+++ remote/main.tf
@@ -2,17 +2,19 @@
 # Configure the AWS Provider
 provider "aws" {
   region     = "ap-southeast-1"
+  access_key = "AKIA32GTTIRLA5S2MNE5"
+  secret_key = "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"
 }
 
-# terraform {
-#   backend "s3" {
-#     bucket     = "rt-tf-test-bucket"
-#     key        = "terraform.tfstate"
-#     region     = "ap-southeast-1"
-#     access_key = "access_key"
-#     secret_key = "secret_key"
-#   }
-# }
+// terraform {
+//   backend "s3" {
+//     bucket     = "rt-tf-test-bucket"
+//     key        = "terraform.tfstate"
+//     region     = "ap-southeast-1"
+//     access_key = "access_key"
+//     secret_key = "secret_key"
+//   }
+// }
 
 terraform {
   backend "remote" {
@@ -20,7 +22,7 @@ terraform {
     organization = "env-tct37d1oddo7u4g"
 
     workspaces {
-      name = "Demo_S3_2_Scalr"
+      name = "03"
     }
   }
 }
@@ -29,10 +31,10 @@ module "ec2-instance-trmodule" {
   source  = "trivbuit.scalr.io/env-tct37d1oddo7u4g/ec2-instance-trmodule/aws"
   version = "1.0.4"
 
-  count = 3
+  count = 2
 
   ami           = "ami-0f86a70488991335e"
-  instance_type = "t2.nano"
+  instance_type = "t2.micro"
 
   key_name = "trbvm"
 }
