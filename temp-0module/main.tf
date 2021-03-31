10bc29826dc70e106bb2f95f708f73315cb84a74
Tri Van BUI <trivbuit@gmail.com>
2021-03-31 10:37:43 +0700

    Add new template 0module

---
+++ temp-0module/main.tf
@@ -0,0 +1,20 @@
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+resource "aws_instance" "template_0module" {
+  ami           = "ami-0f86a70488991335e"
+  instance_type = "t2.micro"
+
+  key_name = "trbvm"
+
+  tags = {
+    Name = "Demo Scalr"
+  }
+}
+
+output "login" {
+  value = aws_instance.web.public_ip
+}
